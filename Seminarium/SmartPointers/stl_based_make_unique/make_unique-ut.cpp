8013a222aa4f646d24e48bbadd94f831fe8c5846
Macko <62703601+MaciejSalamonski@users.noreply.github.com>
2021-01-07 15:52:38 +0100

    Update make_unique-ut.cpp

--- Seminarium/SmartPointers/stl_based_make_unique/make_unique-ut.cpp
+++ Seminarium/SmartPointers/stl_based_make_unique/make_unique-ut.cpp
@@ -1,17 +1,82 @@
-#pragma once
+#include "gtest/gtest.h"
+#include "make_unique.hpp"
 
-#include <memory>
-#include <type_traits>
+constexpr int structTestValue = 1;
+constexpr int structDefaultValue = 2;
+constexpr int structAnotherTestValue = 3;
+constexpr int defaultTestValue = 4;
+constexpr int anotherTestValue = 5;
 
-namespace scr {
+constexpr int lValue = 6;
+constexpr int rValue = 7;
 
-template <typename T, typename... Args, typename = std::enable_if_t<!std::is_array_v<T>>>
-auto make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T{std::forward<Args>(args)...});
+struct TestStruct {
+    TestStruct(int firstValue = structTestValue)
+        : firstValue_{firstValue} {}
+
+    TestStruct(int firstValue, int anotherTestValue)
+        : firstValue_{firstValue}, anotherTestValue_{anotherTestValue} {}
+
+    int firstValue_;
+    int anotherTestValue_{structTestValue};
+};
+
+struct TestStructForLAndRValue {
+    TestStructForLAndRValue()
+        : testValue_{lValue} {}
+
+    TestStructForLAndRValue(const TestStructForLAndRValue&)
+        : TestStructForLAndRValue() {}
+
+    TestStructForLAndRValue(TestStructForLAndRValue&&)
+        : testValue_{rValue} {}
+
+    int testValue_;
+};
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointer) {
+    auto ptr = scr::make_unique<int>(defaultTestValue);
+    EXPECT_EQ(*ptr, defaultTestValue);
 }
 
-template <typename T>
-auto make_unique(size_t size) {
-    return std::unique_ptr<T>(new std::remove_extent_t<T>[size]);
+TEST(MakeUniqueTest, ShouldCreatePointerWithCopyConstructor) {
+    TestStructForLAndRValue copyTest;
+    auto testStructPtr = scr::make_unique<TestStructForLAndRValue>(copyTest);
+    EXPECT_EQ(testStructPtr->testValue_, lValue);
+    EXPECT_EQ(testStructPtr->testValue_, copyTest.testValue_);
 }
+
+TEST(MakeUniqueTest, ShouldCreatePointerWithMoveConstructor) {
+    TestStructForLAndRValue moveTest;
+    auto testStructPtr = scr::make_unique<TestStructForLAndRValue>(std::move(moveTest));
+    EXPECT_EQ(testStructPtr->testValue_, rValue);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerFromStrucktWithDefaultValue) {
+    auto testStructPtr = scr::make_unique<TestStruct>();
+    EXPECT_EQ(testStructPtr->firstValue_, structTestValue);
+    EXPECT_EQ(testStructPtr->anotherTestValue_, structTestValue);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerFromStrucktdefaultTestValue) {
+    auto testStructPtr = scr::make_unique<TestStruct>(structDefaultValue);
+    EXPECT_EQ(testStructPtr->firstValue_, structDefaultValue);
+    EXPECT_EQ(testStructPtr->anotherTestValue_, structTestValue);
+}
+
+TEST(MakeUniqueTest, ShouldCreatedUniquePointerFromStrucktWithParameters) {
+    auto testStructPtr = scr::make_unique<TestStruct>(structDefaultValue,
+                                                      structAnotherTestValue);
+
+    EXPECT_EQ(testStructPtr->firstValue_, structDefaultValue);
+    EXPECT_EQ(testStructPtr->anotherTestValue_, structAnotherTestValue);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerForArray) {
+    constexpr int arraySize = 2;
+    auto arrayPtr = scr::make_unique<int[]>(arraySize);
+    arrayPtr[0] = defaultTestValue;
+    arrayPtr[1] = anotherTestValue;
+    EXPECT_EQ(arrayPtr[0], defaultTestValue);
+    EXPECT_EQ(arrayPtr[1], anotherTestValue);
 }
